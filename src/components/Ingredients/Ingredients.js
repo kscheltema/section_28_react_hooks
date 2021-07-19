28768ec8ff11af1c507edd336e2714e433ddd148
kscheltema <k87scheltema@outlook.com>
2021-07-19 15:55:46 +0200

    useState management prevState

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
+import IngredientList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 
 const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
   return (
     <div className="App">
       <IngredientForm />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} />
       </section>
     </div>
   );
