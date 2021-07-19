2130473e9e4c01dd311bd0b2caf48465c9cda629
kscheltema <k87scheltema@outlook.com>
2021-07-19 15:09:15 +0200

    useState convention with array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  const inputState = useState({ title: "", amount: "" });
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
   //useState always returns (current & update function)
   const submitHandler = (event) => {
     event.preventDefault();
@@ -19,10 +19,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="text"
               id="title"
-              value={inputState[0].title}
+              value={inputState.title}
               onChange={(event) => {
                 const newTitle = event.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   title: newTitle,
                   amount: prevInputState.amount,
                 }));
@@ -34,10 +34,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState[0].amount}
+              value={inputState.amount}
               onChange={(event) => {
                 const newAmount = event.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   amount: newAmount,
                   title: prevInputState.title,
                 }));
