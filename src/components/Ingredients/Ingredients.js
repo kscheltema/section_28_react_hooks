291f5806ca393eda41e8d50ffd3a9ffdaa04642d
kscheltema <k87scheltema@outlook.com>
2021-07-20 10:18:32 +0200

    infinate loop - avoid

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IngredientList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  fetch(
+    "https://burgerbuilder-89b34-default-rtdb.firebaseio.com//ingredients.json"
+  ).then((response) =>
+    response.json().then((responseData) => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    })
+  );
 
   const addIngredientsHandler = (ingredient) => {
     fetch(
