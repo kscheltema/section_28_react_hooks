447589ac8123a7c4dcd9cebdd642597a1bab87ed
kscheltema <k87scheltema@outlook.com>
2021-07-20 10:23:06 +0200

    the dependancy  with useEffect control render

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,22 +5,23 @@ import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
-
-  fetch(
-    "https://burgerbuilder-89b34-default-rtdb.firebaseio.com//ingredients.json"
-  ).then((response) =>
-    response.json().then((responseData) => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
-      }
-      // setUserIngredients(loadedIngredients);
-    })
-  );
+  useEffect(() => {
+    fetch(
+      "https://burgerbuilder-89b34-default-rtdb.firebaseio.com//ingredients.json"
+    ).then((response) =>
+      response.json().then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        // setUserIngredients(loadedIngredients);
+      })
+    );
+  }, []);
 
   const addIngredientsHandler = (ingredient) => {
     fetch(
